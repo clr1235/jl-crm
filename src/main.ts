7219a2da6908847ccc83bf86d60bf5a2e02d3dd9
clr1235 <15035291867@163.com>
2024-01-04 11:39:31 +0800

    chore(root): 使用vue-router

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import router from './router'
 
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(pinia)
-app.mount('#app')
+app.use(router)
+
+router.isReady().then(() => {
+	app.mount('#app')
+})
