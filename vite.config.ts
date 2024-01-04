8785c87c699f957c005b28bfd2c38f2a6f8b79c9
clr1235 <15035291867@163.com>
2024-01-04 10:45:43 +0800

    chore(root): vite配置文件修改

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -15,4 +16,56 @@ export default defineConfig({
 			resolvers: [ElementPlusResolver()],
 		}),
 	],
+	server: {
+		host: '0.0.0.0', // 允许IP访问
+		// 开发代理
+		proxy: {
+			'/api': {
+				target: 'https://mock.apifox.cn/m1/3331204-0-default',
+				secure: true, // 如果是https接口，需要配置这个参数为true
+				changeOrigin: true, // 如果接口跨域，需要进行这个参数配置为true
+				rewrite: (path) => path.replace(/^\/api/, ''),
+			},
+			'/crm': {
+				// target: 'https://testjlc.kcwl.com', // 建龙5.3测试环境
+				target: 'https://testcrm.jlkc56.com', // 建龙5.2测试环境
+				// target: 'https://testnmc.kcwl.com', // 内蒙测试环境
+				// target: 'http://172.16.50.74:8081',
+				// target: 'https://precrm.jlkc56.com', // 建龙二套测试环境
+				// target: 'https://testcrm215.jlkc56.com', // 乌海测试环境
+				changeOrigin: true,
+				// timeout: 3000000, // 5分钟
+			},
+			'/preference': {
+				// target: 'https://testjlc.kcwl.com', // 建龙5.3测试环境
+				target: 'https://testcrm.jlkc56.com', // 建龙5.2测试环境
+				// target: 'https://testnmc.kcwl.com', // 内蒙测试环境
+				// target: 'http://http://192.168.1.156:9818/', // 谭祥美
+				// target: 'http://172.16.50.74:8081',
+				// target: 'https://preapi.jlkc56.com/', //第二套测试环境
+				changeOrigin: true,
+				// timeout: 3000000, // 5分钟
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	// 构建
+	build: {
+		chunkSizeWarningLimit: 2000, // 消除打包大小超过500kb警告
+		minify: 'terser', // Vite 2.6.x 以上需要配置 minify: "terser", terserOptions 才能生效
+		terserOptions: {
+			compress: {
+				keep_infinity: true, // 防止 Infinity 被压缩成 1/0，这可能会导致 Chrome 上的性能问题
+				drop_console: true, // 生产环境去除 console
+				drop_debugger: true, // 生产环境去除 debugger
+			},
+			format: {
+				comments: false, // 删除注释
+			},
+		},
+	},
 })
